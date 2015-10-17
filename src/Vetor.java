e5ebe5ac46c56a7f068e4d75d370a097c5d2167d
Andre Almeida <andre.almeida@msn.com>
2015-10-17 18:31:40 -0300

    Create class Vetor

---
+++ src/Vetor.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Vetor {
+    private int vetorNumber[];
+
+
+    public Vetor() {
+        this.vetorNumber = new int[10];
+        geraNUmero();
+    }
+
+    private void geraNUmero(){
+        Random ramdomNumber = new Random();
+        for (int i = 0; i < 10; i++) {
+            this.vetorNumber[i] = ramdomNumber.nextInt(20);
+        }
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+        public static void main(String[] args) {
+
+         Vetor vetor = new Vetor();
+            //vetor.geraNUmero();
+
+
+
+
+        }
+
+    }
+
+
